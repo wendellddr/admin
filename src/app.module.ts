b576f79332a3838d696a9cd0b5cf69739f6338dc
Wendell Douglas <wendell.douglas@marketeasy.store>
2022-03-01 22:13:03 -0300

    New project admin

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { LoginModule } from './login/login.module';
 import * as ormconfig from '../ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({ ...ormconfig }), UsersModule],
+  imports: [TypeOrmModule.forRoot({ ...ormconfig }), UsersModule, LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
